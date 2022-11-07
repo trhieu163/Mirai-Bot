b1d89f49cf5a90234188ff69432992850cfe3443
HieuSimpRin <117310218+HieuSimpRin@users.noreply.github.com>
2022-11-07 20:45:28 +0700

    antiout.js

---
+++ mô-đun/sự kiện/antiout.js
@@ -0,0 +1,36 @@
+/**
+* @author ProCoderMew
+* @warn Do not edit code or edit credits
+*/
+
+module.exports.config = {
+    name: "antiout",
+    eventType: ["log:unsubscribe"],
+    version: "1.0.7",
+    credits: "ProCoderMew",
+    description: "Listen events",
+    dependencies: {
+        "path": ""
+    }
+};
+
+module.exports.run = async function ({ api, event, Users }) {
+    const { resolve } = global.nodemodule["path"];
+    const path = resolve(__dirname, '../commands', 'cache', 'meewmeew.json');
+    const { antiout } = require(path);
+    const { logMessageData, author, threadID } = event;
+    const id = logMessageData.leftParticipantFbId;
+    if (author == id && id != api.getCurrentUserID()) {
+        const name = await Users.getNameUser(id) || "Người dùng Facebook";
+        if (antiout.hasOwnProperty(threadID) && antiout[threadID] == true) {
+            try {
+                await api.addUserToGroup(id, threadID);
+                return api.sendMessage(`Đã thêm ${name} vào lại nhóm .`);
+            }
+            catch (e) {
+                return api.sendMessage(`🚫Không thể thêm ${name} vừa out vào lại nhóm😢.`, threadID);
+            }
+        }
+    }
+    return;
+}
